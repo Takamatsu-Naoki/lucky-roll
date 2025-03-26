235b8d4cfadb75b68ca85880a104e8bd9fddc651
Takamatsu Naoki <Takamatsu0331@outlook.jp>
2025-03-26 21:04:23 +0900

    :tada: Initial Commit

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Development and build setup for 'lucky-roll', a simple dice roller game in Rust";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        lib = pkgs.lib;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages =  with pkgs; [
+            rustc
+            cargo
+          ];
+        };
+
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "lucky-roll";
+          version = "0.1.0";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+          meta = {
+            description = "A simple dice roller that keeps spinning until you hit a triple.";
+            homepage = "https://github.com/Takamatsu-Naoki/lucky-roll";
+            license = lib.licenses.mit;
+            platforms = lib.platforms.all;
+          };
+        };
+      });
+}
